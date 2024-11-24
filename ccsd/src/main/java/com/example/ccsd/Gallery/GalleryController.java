6e4951a5d727709ce9c5a3c33e940af4b691fba8
fsihamzh <ciha191125@gmail.com>
2024-11-25 01:44:40 +0800

    change api name

--- ccsd/src/main/java/com/example/ccsd/Gallery/GalleryController.java
+++ ccsd/src/main/java/com/example/ccsd/Gallery/GalleryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/Gallery")
+@RequestMapping("/api/gallery")
 public class GalleryController {
     
     @Autowired
--- ccsd/src/main/java/com/example/ccsd/Products/ProductsController.java
+++ ccsd/src/main/java/com/example/ccsd/Products/ProductsController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/Products")
+@RequestMapping("/api/products")
 public class ProductsController {
     
     @Autowired
