0c51895445e35b876ef8eadc48a28058269bd224
fsihamzh <ciha191125@gmail.com>
2024-11-25 04:29:44 +0800

    hardcode

--- dashboard/src/landingpage/services.jsx
+++ dashboard/src/landingpage/services.jsx
@@ -6,9 +6,7 @@ export const Services = (props) => {
       <div className="container">
         <div className="section-title">
           <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          <p>At Puff Lab, we specialize in crafting premium Japanese cream puffs with a delightful twist, providing an unforgettable taste experience for every occasion. From individual indulgences to catering for events, we ensure the highest quality and freshness in every bite.
           </p>
         </div>
         <div className="row">
@@ -16,7 +14,7 @@ export const Services = (props) => {
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
                   {" "}
-                  <i className={d.icon}></i>
+                  <img src={d.img} alt="..." className="team-img" />
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <h4>{d.price}</h4>
